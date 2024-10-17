7cf0d4208499061f01885c2d21a9109113189f5a
Ruano Jama Jharol <101405632+Hikigaya-Hachiman-2003@users.noreply.github.com>
2024-10-16 22:40:19 -0500

    version 1

--- practica-angular/src/app/pages/pokemon/card/card.component.ts
+++ practica-angular/src/app/pages/pokemon/card/card.component.ts
@@ -1,33 +1,23 @@
-import { Component, Input, OnChanges, SimpleChanges, ViewChild } from '@angular/core';
-import { Pokemon, Pokemons } from '../interfaces/pokemons';
+import { Component, Input, OnChanges, SimpleChanges } from '@angular/core';
+import { Pokemons } from '../interfaces/pokemons';
 import { NgFor, NgIf } from '@angular/common';
-import { ModalComponent } from '../modal/modal.component';
 
 @Component({
   selector: 'pokemon-card',
   standalone: true,
-  imports: [NgIf, NgFor, ModalComponent],
+  imports: [NgIf, NgFor],
   templateUrl: './card.component.html',
   styleUrl: './card.component.css'
 })
 export class CardComponent implements OnChanges {
   @Input() public pokemonsAll: Pokemons | undefined;
 
-  @ViewChild(ModalComponent) public modal!: ModalComponent
-
   imageLoaded: boolean = false;
-
-  selectedPokemon!: Pokemon
-
+  
   ngOnChanges(changes: SimpleChanges): void {
     if (changes['pokemonsAll']) {
-      this.imageLoaded = false;
+
     }
   }
 
-  openModal(pokemon: Pokemon):void {
-    if(this.modal){
-      this.modal.open(pokemon)
-    }
-  }
 }
--- practica-angular/src/app/pages/pokemon/paginacion/paginacion.component.ts
+++ practica-angular/src/app/pages/pokemon/paginacion/paginacion.component.ts
@@ -1,44 +1,12 @@
-
-import { Component, Output, EventEmitter} from '@angular/core';
-import { PokemonService } from '../services/pokemon.service';
-import { NgClass } from '@angular/common';
-import { Pokemons } from '../interfaces/pokemons';
+import { Component } from '@angular/core';
 
 @Component({
-  selector: 'pokemon-paginacion',
+  selector: 'app-paginacion',
   standalone: true,
-  imports: [NgClass],
+  imports: [],
   templateUrl: './paginacion.component.html',
   styleUrl: './paginacion.component.css'
 })
 export class PaginacionComponent {
-
-  @Output() public eventNewPokemons = new EventEmitter<Pokemons>();
-
-  constructor (
-    private _srvPokemon: PokemonService
-  ) {}
-
-  get nextURL(): string | null {
-    return this._srvPokemon.nextURL;
-  }
-
-  get previousURL(): string | null {
-    return this._srvPokemon.previousURL;
-  }
-
-  loadPokemons(url:string){
-    this._srvPokemon.getPokemons(url).subscribe((pokemonsAll)=>{
-      pokemonsAll.results.forEach((pokemon)=>{
-        this._srvPokemon.getpokemon(pokemon.name).subscribe((pokemonData)=>{
-          pokemon.data = pokemonData;
-          this._srvPokemon.nextURL = pokemonsAll.next;
-          this._srvPokemon.previousURL = pokemonsAll.previous;
-          this.eventNewPokemons.emit(pokemonsAll);
-        })
-      })
-
-    });
-  }
 
 }
