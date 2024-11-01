98fa033b49c8112d8969859beaf1ccb77d9e5ed2
Ruano Jama Jharol <101405632+Hikigaya-Hachiman-2003@users.noreply.github.com>
2024-11-01 11:46:09 -0500

    version 15
    creacion de metodos, get, put, post y delete

--- misapis/server/server.js
+++ misapis/server/server.js
@@ -1,18 +1,26 @@
 import express from 'express'
 import cors from 'cors'
 import indexRoutes from '../routes/index.routes.js'
+import *as db from '../db/cnn_mongodb.js'
 
 export default class Server {
     constructor() {
         this.app = express()
-        this.port = 3000
+        this.port = process.env.PORT || 3000
         this.generalRoute = '/api/'
 
+        this.conectarDBMongo()
         //Middlewares
         this.middlewares()
 
         //Ruta de mi aplicacion
         this.routes()
+    }
+
+    async conectarDBMongo(){
+        if(!db.isConnected){
+            await db.conectarAMongoDB()
+        }
     }
 
     middlewares() {
@@ -29,11 +37,16 @@ export default class Server {
     routes() {
        // localhost: 000/api/ejemplo
         this.app.use(this.generalRoute, indexRoutes)
+        //emnsaje de error en json
+        this.app.use('**', (req, res) => {
+            res.status(404).json({
+                msg: 'Not found'})
+        })
     }
 
     listen() {
         this.app.listen(this.port, () => {
-            console.log(`Server running on port ${this.port}`)
+            console.log(`Server running on port ${this.port}`.yellow)
         })
     }
 }
